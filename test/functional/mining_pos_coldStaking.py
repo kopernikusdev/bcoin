7f026240c55fcd873ac23a0f40fc9803e3776eba
kopernikusdev <kopernikusdev@lotzi.net>
2020-12-30 19:35:44 +0100

    Nuke zerocoin from the functional test framework

--- test/functional/mining_pos_coldStaking.py
+++ test/functional/mining_pos_coldStaking.py
@@ -37,6 +37,7 @@ class BALLCOIN_ColdStakingTest(BALLcoinTestFramework):
 
     def setup_chain(self):
         # Start with PoW cache: 200 blocks
+        self.log.info("Initializing test directory " + self.options.tmpdir)
         self._initialize_chain()
         self.enable_mocktime()
 
